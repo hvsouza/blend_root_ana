c466e3a22800dba83523e4bf427f29ab2d4cbc65
Henrique Souza PC casa <henriquevieira.souza@gmail.com>
2022-01-27 14:54:35 +0100

    updated filter to version 2

--- cold_box_analysis/class/readingCodes.C
+++ cold_box_analysis/class/readingCodes.C
@@ -4,52 +4,134 @@
 class DENOISE{
   
 public:
-  
-  template<class T>
-  void TV1D_denoise(T* input, T* output, const int width, const float lambda) {
-    if (width>0) {				/*to avoid invalid memory access to input[0]*/
-      int k=0, k0=0;			/*k: current sample location, k0: beginning of current segment*/
-      float umin=lambda, umax=-lambda;	/*u is the dual variable*/
-      float vmin=input[0]-lambda, vmax=input[0]+lambda;	/*bounds for the segment's value*/
-      int kplus=0, kminus=0;  /*last positions where umax=-lambda, umin=lambda, respectively*/
-      const float twolambda=2.0*lambda;	/*auxiliary variable*/
-      const float minlambda=-lambda;		/*auxiliary variable*/
-      for (;;) {				/*simple loop, the exit test is inside*/
-        while (k==width-1) {	/*we use the right boundary condition*/
-          if (umin<0.0) {			/*vmin is too high -> negative jump necessary*/
-            do output[k0++]=vmin; while (k0<=kminus);
-            umax=(vmin=input[kminus=k=k0])+(umin=lambda)-vmax;
-          } else if (umax>0.0) {	/*vmax is too low -> positive jump necessary*/
-            do output[k0++]=vmax; while (k0<=kplus);
-            umin=(vmax=input[kplus=k=k0])+(umax=minlambda)-vmin;
-          } else {
-            vmin+=umin/(k-k0+1);
-            do output[k0++]=vmin; while(k0<=k);
-            return;
-          }
+    template<class T>
+    void TV1D_denoise(T* input, T* output, unsigned int width, const Double_t lambda) {
+
+        unsigned int* indstart_low = (unsigned int*) malloc(sizeof *indstart_low * width);
+        unsigned int* indstart_up = (unsigned int*) malloc(sizeof *indstart_up * width);
+        unsigned int j_low = 0, j_up = 0, jseg = 0, indjseg = 0, i=1, indjseg2, ind;
+        double output_low_first = input[0]-lambda;
+        double output_low_curr = output_low_first;
+        double output_up_first = input[0]+lambda;
+        double output_up_curr = output_up_first;
+        const double twolambda=2.0*lambda;
+        if (width==1) {output[0]=input[0]; return;}
+        indstart_low[0] = 0;
+        indstart_up[0] = 0;
+        width--;
+        for (; i<width; i++) {
+            if (input[i]>=output_low_curr) {
+                if (input[i]<=output_up_curr) {
+                    output_up_curr+=(input[i]-output_up_curr)/(i-indstart_up[j_up]+1);
+                    output[indjseg]=output_up_first;
+                    while ((j_up>jseg)&&(output_up_curr<=output[ind=indstart_up[j_up-1]]))
+                        output_up_curr+=(output[ind]-output_up_curr)*
+                            ((double)(indstart_up[j_up--]-ind)/(i-ind+1));
+                    if (j_up==jseg) {
+                        while ((output_up_curr<=output_low_first)&&(jseg<j_low)) {
+                            indjseg2=indstart_low[++jseg];
+                            output_up_curr+=(output_up_curr-output_low_first)*
+                                ((double)(indjseg2-indjseg)/(i-indjseg2+1));
+                            while (indjseg<indjseg2) output[indjseg++]=output_low_first;
+                            output_low_first=output[indjseg];
+                        }
+                        output_up_first=output_up_curr;
+                        indstart_up[j_up=jseg]=indjseg;
+                    } else output[indstart_up[j_up]]=output_up_curr;
+                } else
+                    output_up_curr=output[i]=input[indstart_up[++j_up]=i];
+                output_low_curr+=(input[i]-output_low_curr)/(i-indstart_low[j_low]+1);
+                output[indjseg]=output_low_first;
+                while ((j_low>jseg)&&(output_low_curr>=output[ind=indstart_low[j_low-1]]))
+                    output_low_curr+=(output[ind]-output_low_curr)*
+                            ((double)(indstart_low[j_low--]-ind)/(i-ind+1));
+                if (j_low==jseg) {
+                    while ((output_low_curr>=output_up_first)&&(jseg<j_up)) {
+                        indjseg2=indstart_up[++jseg];
+                        output_low_curr+=(output_low_curr-output_up_first)*
+                            ((double)(indjseg2-indjseg)/(i-indjseg2+1));
+                        while (indjseg<indjseg2) output[indjseg++]=output_up_first;
+                        output_up_first=output[indjseg];
+                    }
+                    if ((indstart_low[j_low=jseg]=indjseg)==i) output_low_first=output_up_first-twolambda;
+                    else output_low_first=output_low_curr;
+                } else output[indstart_low[j_low]]=output_low_curr;
+            } else {
+                output_up_curr+=((output_low_curr=output[i]=input[indstart_low[++j_low] = i])-
+                    output_up_curr)/(i-indstart_up[j_up]+1);
+                output[indjseg]=output_up_first;
+                while ((j_up>jseg)&&(output_up_curr<=output[ind=indstart_up[j_up-1]]))
+                    output_up_curr+=(output[ind]-output_up_curr)*
+                            ((double)(indstart_up[j_up--]-ind)/(i-ind+1));
+                if (j_up==jseg) {
+                    while ((output_up_curr<=output_low_first)&&(jseg<j_low)) {
+                        indjseg2=indstart_low[++jseg];
+                        output_up_curr+=(output_up_curr-output_low_first)*
+                            ((double)(indjseg2-indjseg)/(i-indjseg2+1));
+                        while (indjseg<indjseg2) output[indjseg++]=output_low_first;
+                        output_low_first=output[indjseg];
+                    }
+                    if ((indstart_up[j_up=jseg]=indjseg)==i) output_up_first=output_low_first+twolambda;
+                    else output_up_first=output_up_curr;
+                } else output[indstart_up[j_up]]=output_up_curr;
+            }
         }
-        if ((umin+=input[k+1]-vmin)<minlambda) {		/*negative jump necessary*/
-          do output[k0++]=vmin; while (k0<=kminus);
-          vmax=(vmin=input[kplus=kminus=k=k0])+twolambda;
-          umin=lambda; umax=minlambda;
-        } else if ((umax+=input[k+1]-vmax)>lambda) {	/*positive jump necessary*/
-          do output[k0++]=vmax; while (k0<=kplus);
-          vmin=(vmax=input[kplus=kminus=k=k0])-twolambda;
-          umin=lambda; umax=minlambda;
-        } else {        /*no jump necessary, we continue*/
-          k++;
-          if (umin>=lambda) {		/*update of vmin*/
-            vmin+=(umin-lambda)/((kminus=k)-k0+1);
-            umin=lambda;
-          }
-          if (umax<=minlambda) {	/*update of vmax*/
-            vmax+=(umax+lambda)/((kplus=k)-k0+1);
-            umax=minlambda;
-          }
+        /* here i==width (with value the actual width minus one) */
+        if (input[i]+lambda<=output_low_curr) {
+            while (jseg<j_low) {
+                indjseg2=indstart_low[++jseg];
+                while (indjseg<indjseg2) output[indjseg++]=output_low_first;
+                output_low_first=output[indjseg];
+            }
+            while (indjseg<i) output[indjseg++]=output_low_first;
+            output[indjseg]=input[i]+lambda;
+        } else if (input[i]-lambda>=output_up_curr) {
+            while (jseg<j_up) {
+                indjseg2=indstart_up[++jseg];
+                while (indjseg<indjseg2) output[indjseg++]=output_up_first;
+                output_up_first=output[indjseg];
+            }
+            while (indjseg<i) output[indjseg++]=output_up_first;
+            output[indjseg]=input[i]-lambda;
+        } else {
+            output_low_curr+=(input[i]+lambda-output_low_curr)/(i-indstart_low[j_low]+1);
+            output[indjseg]=output_low_first;
+            while ((j_low>jseg)&&(output_low_curr>=output[ind=indstart_low[j_low-1]]))
+                output_low_curr+=(output[ind]-output_low_curr)*
+                            ((double)(indstart_low[j_low--]-ind)/(i-ind+1));
+            if (j_low==jseg) {
+                if (output_up_first>=output_low_curr)
+                    while (indjseg<=i) output[indjseg++]=output_low_curr;
+                else {
+                    output_up_curr+=(input[i]-lambda-output_up_curr)/(i-indstart_up[j_up]+1);
+                    output[indjseg]=output_up_first;
+                    while ((j_up>jseg)&&(output_up_curr<=output[ind=indstart_up[j_up-1]]))
+                        output_up_curr+=(output[ind]-output_up_curr)*
+                            ((double)(indstart_up[j_up--]-ind)/(i-ind+1));
+                    while (jseg<j_up) {
+                        indjseg2=indstart_up[++jseg];
+                        while (indjseg<indjseg2) output[indjseg++]=output_up_first;
+                        output_up_first=output[indjseg];
+                    }
+                    indjseg=indstart_up[j_up];
+                    while (indjseg<=i) output[indjseg++]=output_up_curr;
+                }
+            } else {
+                while (jseg<j_low) {
+                    indjseg2=indstart_low[++jseg];
+                    while (indjseg<indjseg2) output[indjseg++]=output_low_first;
+                    output_low_first=output[indjseg];
+                }
+                indjseg=indstart_low[j_low];
+                while (indjseg<=i) output[indjseg++]=output_low_curr;
+            }
         }
-      }
+        free(indstart_low);
+        free(indstart_up);
     }
-  }
+
+
+
 };
 
 
