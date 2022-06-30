8fc1daf0e15a1f256663967d7005f43fedeeb16e
hvsouza <henriquevieira.souza@gmail.com>
2022-06-30 19:31:39 +0200

    I am trying to understand the proportion of negative and positive

--- deconvolution_wiener_filter/deconvolution_show_off.C
+++ deconvolution_wiener_filter/deconvolution_show_off.C
@@ -28,23 +28,29 @@ void deconvolution_show_off(){
   wdec.baseline = 4400;
   Bool_t startfilling = false; // to avoid the first signal oscillation
   Int_t start_sphe = 0;
-  Double_t sphe_pretrigger_ticks = 62;
-  // Double_t sphe_pretrigger_ticks = 68;
+  // Double_t sphe_pretrigger_ticks = 0;
+  Double_t sphe_pretrigger_ticks = 68;
+  // Double_t sphe_pretrigger_ticks = 62;
   Double_t offset = 5800-sphe_pretrigger_ticks*dtime;
- Double_t sphe_final_time = 6300;
+  Double_t sphe_final_time = 6200;
   // Double_t sphe_final_time = 625*dtime;
 
 
   wsignal.maxBin = offset/dtime+3;
+  Double_t sphe_integral = 0;
   for(Int_t i=0; i<memorydepth; i++){
-    if(i>sphe_pretrigger_ticks && *(gsphe->GetY()+i)>=0 && startfilling==false){
+    Double_t sphe_val = *(gsphe->GetY()+i);
+    if(i>sphe_pretrigger_ticks && sphe_val>=0 && startfilling==false){
       startfilling = true;
       start_sphe = i;
     }
     if(startfilling){
-      if(i<6200/dtime)
-      // if(i<625)
-        hsphe->SetBinContent(i+1,*(gsphe->GetY()+i));
+      if(i<sphe_final_time/dtime){        
+        hsphe->SetBinContent(i+1,sphe_val);
+        // cout << sphe_val << endl;
+        if(sphe_val>0)
+          sphe_integral+=sphe_val*dtime;
+      }
     }
     // hsphe->SetBinContent(i+1,func_fit->Eval(i*dtime));
   }
@@ -54,21 +60,33 @@ void deconvolution_show_off(){
   wpe.fft(hsphe);
   wfilter.wienerGenFilter(wpe,wnoise,wsignal);
 
-  gRandom->SetSeed(0);
+  gRandom->SetSeed(1);
   vector<Double_t> ph_time;
   vector<TLine *> ph_line;
   Double_t fast = 7, slow = 1400;
   Double_t prob_fast = 0.23;
   Double_t prob_slow = 1-prob_fast;
- 
-  
+  Int_t nphotons = 1000;
+  Double_t signal_integral = 0;
+
+  Double_t time_of_change = 0;
+
+  TF1 *fplar = new TF1("fplar","[0]*exp(-x/[1])/[1]+[2]*exp(-x/[3])/[3]",0,10000);
+  fplar->SetParameters(prob_fast,fast,prob_slow,slow);
+
+  vector<Double_t> ratio_det;
+  vector<Double_t> ratio_int;
+  vector<Double_t> ratio_count;
+  Double_t photons_count = 0;
+  Double_t func_int = 0;
   for(Int_t event = 0; event<1; event++){
     hsignal->Reset();
     // hexp->Reset();
     // havg->Reset();
     // hres->Reset();
-    // Int_t nphotons = gRandom->Poisson(200);
-    Int_t nphotons = 20;
+    // nphotons = gRandom->Poisson(200);
+    // nphotons = gRandom->Uniform(200,1000);
+    nphotons = 500;
     ph_time.resize(nphotons,0);
     ph_line.resize(nphotons,0);
       
@@ -113,23 +131,54 @@ void deconvolution_show_off(){
     wsignal.fft(hsignal);
     wdec.frequency_deconv(wsignal,wfilter);
     
-
+    Bool_t check_change = true;
+    signal_integral = 0;
     for(Int_t i=0; i<memorydepth; i++){
+      Double_t signal_val = hsignal->GetBinContent(i+1);
       hres->AddBinContent(i+1,wdec.hwvf->GetBinContent(i+1));
+      if(signal_val>0)signal_integral+=signal_val*dtime;
+      if(i>6000/4 && check_change){
+        if(signal_val<=0){
+          time_of_change = i*dtime;
+          check_change = false;
+        }
+      }
     }
     cout << event << endl;
+    photons_count = 0;
+    for(Int_t i=0; i<nphotons; i++){
+      if(ph_time[i]<time_of_change){
+        photons_count+=1;
+      }
+    }
+    func_int = fplar->Integral(0,time_of_change-offset);
+    ratio_count.push_back(photons_count/nphotons);
+    ratio_int.push_back(func_int);
+    ratio_det.push_back(signal_integral/sphe_integral/nphotons);
+ 
   }
 
+  
+
+  cout << "sphe: " << sphe_integral << endl;
+  cout << "signal: " << signal_integral << endl;
+  cout << "n: " << signal_integral/sphe_integral << endl;
+  cout << "start: " << offset << endl;
+  cout << "finish: " << time_of_change << endl;
+  cout << "integrated: " << func_int << endl;
+  cout << "count photons: " << photons_count << endl;
+  
   Double_t maxVal = hsignal->GetMaximum();
   // hsignal->Scale(1/maxVal);
 
 
-  // wpe.shift_waveform(hsphe,6000/4.);
+  // wdec.shift_waveform(hres,offset/4);
   maxVal = hsphe->GetMaximum();
   // hsphe->Scale(1/maxVal);
 
   TCanvas *c1 = new TCanvas("c1","testing plataform");
   hsignal->Draw();
+  // fplar->Draw();
   TCanvas *cexp = new TCanvas("cexp","expected waveform");
   hexp->Draw();
   for (const auto &line : ph_line) line->Draw("same");
@@ -153,8 +202,13 @@ void deconvolution_show_off(){
   hexp->SetLineColor(kRed);
   hexp->Draw("SAME");
 
-  
-  
+
+  TCanvas *c3 = new TCanvas("c3","ratio by integral");
+  TGraph *gr1 = new TGraph(ratio_int.size(),&ratio_count[0], &ratio_int[0]);
+  gr1->Draw("AP");
+  TCanvas *c4 = new TCanvas("c4","ratio by detection");
+  TGraph *gr2 = new TGraph(ratio_int.size(),&ratio_count[0], &ratio_det[0]);
+  gr2->Draw("AP");
 
 }
  
