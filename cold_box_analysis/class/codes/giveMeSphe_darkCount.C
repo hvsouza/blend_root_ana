5a01dbb2d793bdc62776a53b2985d37ebe6fdab4
Henrique Souza <henriquevieira.souza@gmail.com>
2022-10-11 12:49:39 +0200

    better comments givemespe

--- cold_box_analysis/class/codes/giveMeSphe_darkCount.C
+++ cold_box_analysis/class/codes/giveMeSphe_darkCount.C
@@ -17,30 +17,30 @@ void giveMeSphe_darkCount(){
     dark.just_a_test = false;
     dark.just_this = 200;
     
-    dark.tolerance = 5; // n sigmas (smoothed)
-    dark.baseLimit = 3; // higher then this wont contribute to the baseline abs(baseLimit)
-    dark.baselineTime = 5000;
+    dark.tolerance = 5; // n sigmas (smoothed) (not used for led)
+    dark.baseLimit = 3; // higher then this wont contribute to the baseline abs(baseLimit) (not used for led)
+    dark.baselineTime = 5000; // (not used for led)
 
     dark.start = 9950; // start the search for peaks or start the integration (led)
     dark.finish = 10250; // fisish the search or finish the integration (led)
 
-    dark.timeLimit = 0; // time after LED signal
-    dark.timeLow = 60; // integration time before peak
-    dark.timeHigh = 400; // integration time after peak
+    dark.timeLimit = 0; // time after LED signal (not used for led)
+    dark.timeLow = 60; // integration time before peak (not used for led)
+    dark.timeHigh = 400; // integration time after peak (not used for led)
     
-    dark.lowerThreshold = -1; // threshold to detect noise (normal waveform)
-    dark.maxHits = 1; // maximum hit before discarding  
+    dark.lowerThreshold = -1; // threshold to detect noise (normal waveform) (not used for led)
+    dark.maxHits = 1; // maximum hit before discarding   (not used for led)
     
     dark.too_big = 1000; // if there is a peak > "too_big" .. wait "waiting" ns
     dark.waiting = 1000;
     
-    dark.filter = 14;
-    dark.interactions = 45; // for moving avarage
-    dark.shifter = 20;
+    dark.filter = 14; // one dimentional denoise filter (0 equal no filder)
+    dark.interactions = 45; // for moving avarage filter (not used on led)
+    dark.shifter = 20; // shift of moving average to compute derivative
 
-    dark.dtime = 4.;
+    dark.dtime = 4.; // time step in ns
 
-    dark.get_wave_form = false;
+    dark.get_wave_form = false; // for getting spe waveforms
     dark.mean_before = 120; // time recorded before and after the peak found 
     dark.mean_after = 1000;
     dark.sphe_charge_ch0 = 1809.52; // wave0
