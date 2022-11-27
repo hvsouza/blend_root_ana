360a04afaebe97b51fbfe012c64c37ae37a82958
Henrique Souza <henriquevieira.souza@gmail.com>
2022-11-27 21:52:01 +0100

    Indentation fix

--- cold_box_analysis/class/variables.C
+++ cold_box_analysis/class/variables.C
@@ -4,18 +4,24 @@
 // Created: 2021
 // ________________________________________ //
 class ADC_DATA{
-public:
-  Double_t peak;
-  Double_t peakpos;
-  Double_t charge;
-  Double_t fprompt;
-  Double_t event;
-  Double_t time;
-  Double_t wvf[memorydepth];
-  Int_t selection;
+  public:
+    Double_t peak;
+    Double_t peakpos;
+    Double_t charge;
+    Double_t fprompt;
+    Double_t event;
+    Double_t time;
+    Double_t wvf[memorydepth];
+    Int_t selection;
     
-  ADC_DATA();
-  const char *tobranch = Form("peak/D:peakpos/D:charge/D:fprompt/D:event/D:time/D:wvf[%i]/D:selection/I",memorydepth);
+    ADC_DATA();
+    string tobranch = "peak/D:peakpos/D:charge/D:fprompt/D:event/D:time/D:wvf[" + to_string(memorydepth) + "]/D:selection/I";
+
+    void setBranchName(Int_t n = memorydepth){
+      tobranch = "peak/D:peakpos/D:charge/D:fprompt/D:event/D:time/D:wvf[" + to_string(n) + "]/D:selection/I";
+    }
+
+    // const char *tobranch = Form("peak/D:peakpos/D:charge/D:fprompt/D:event/D:time/D:wvf[%i]/D:selection/I",memorydepth);
 };
 
 
@@ -32,5 +38,3 @@ ADC_DATA::ADC_DATA(){
   time = 0;
   selection = 0; 
 }
-  
-
