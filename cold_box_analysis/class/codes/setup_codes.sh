7f836d82c73753faa5bd63b88be0a7afe3da3cae
Henrique Souza <henriquevieira.souza@gmail.com>
2023-01-17 17:19:43 +0100

    Also change the load library

--- cold_box_analysis/class/codes/setup_codes.sh
+++ cold_box_analysis/class/codes/setup_codes.sh
@@ -15,3 +15,19 @@ if [ -z "$linenuber" ]; then
 sed -i "$linenumber d" $f
 sed -i "$linenumber i #include \"$class_dir/MYCODES.h\"" $f
 done
+
+
+cd ../
+COUNTER=0
+lines=$(eval "sed -n '/define/=' load_my_class.sh")
+
+for line in $lines; do
+    sed -i "$linenumber d" load_my_class.sh
+    if [[COUNTER==0]]; then
+        sed -i "$linenumber i \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' \" load_my_class.sh" load_my_class.sh
+    else
+        sed -i "$linenumber i \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' \" load_my_class.sh" load_my_class.sh
+    fi
+    COUNTER=$(( COUNTER + 1 ))
+
+done
