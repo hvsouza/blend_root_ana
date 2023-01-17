3a396f3eba15ef90a2094bca14699e226a001e54
Henrique Souza <henriquevieira.souza@gmail.com>
2023-01-17 18:14:58 +0100

    Bug fixing

--- cold_box_analysis/class/codes/setup_codes.sh
+++ cold_box_analysis/class/codes/setup_codes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-current_dir=$pwd
+current_dir=( $pwd )
 cd ../
-class_dir=$pwd
+class_dir=( $pwd )
 cd -
 
 files=$(/bin/ls -1 | grep .C)
@@ -10,7 +10,7 @@ files=$(/bin/ls -1 | grep .C)
 for f in $files; do
     #include "/home/henrique/Dropbox/APC_Paris/Root/cold_box_analysis/class/MYCODES.h"
     linenumber=$(eval "sed -n '/MYCODES.h/=' $f") # search line with pathern
-    if [ -z "$linenuber" ]; then
+    if [ -z "$linenumber" ]; then
         continue
     fi
     sed -i "$linenumber d" $f
@@ -24,7 +24,7 @@ lines=$(eval "sed -n '/define/=' load_my_class.sh")
 
 for line in $lines; do
     sed -i "$linenumber d" load_my_class.sh
-    if [[COUNTER==0]]; then
+    if [[ $COUNTER==0 ]]; then
         sed -i "$linenumber i \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' \" load_my_class.sh" load_my_class.sh
     else
         sed -i "$linenumber i \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' \" load_my_class.sh" load_my_class.sh
