1cc21dc92078d76d6e6643a294887311446df5c7
Henrique Souza <henriquevieira.souza@gmail.com>
2023-01-17 18:50:49 +0100

    More bug fixing

--- cold_box_analysis/class/codes/setup_codes.sh
+++ cold_box_analysis/class/codes/setup_codes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-current_dir=( $pwd )
+current_dir=$( pwd )
 cd ../
-class_dir=( $pwd )
+class_dir=$( pwd )
 cd -
 
 files=$(/bin/ls -1 | grep .C)
@@ -23,11 +23,11 @@ COUNTER=0
 lines=$(eval "sed -n '/define/=' load_my_class.sh")
 
 for line in $lines; do
-    sed -i "$linenumber d" load_my_class.sh
-    if [[ $COUNTER==0 ]]; then
-        sed -i "$linenumber i \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' \" load_my_class.sh" load_my_class.sh
+    sed -i "$line d" load_my_class.sh
+    if [ $COUNTER -eq 0 ]; then
+        sed -i "$line i \ \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' " load_my_class.sh
     else
-        sed -i "$linenumber i \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\"' \" load_my_class.sh" load_my_class.sh
+        sed -i "$line i \ \ \ \ eval 'root -e \"#define memorydepth \$npts\" -e \".L \$class_dir/MYCODES.h\" -e \"SAMPLE s\(\\\"s\\\",\\\"\$file\")"' " load_my_class.sh
     fi
     COUNTER=$(( COUNTER + 1 ))
 
