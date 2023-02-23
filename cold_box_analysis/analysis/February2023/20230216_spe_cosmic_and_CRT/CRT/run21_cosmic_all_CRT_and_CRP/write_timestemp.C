3ddb9dfb21e0ae6a37f39c27a4178f00ae37e771
Henrique Souza <henriquevieira.souza@gmail.com>
2023-02-23 14:59:56 +0100

    Backing up February analysis

---
+++ cold_box_analysis/analysis/February2023/20230216_spe_cosmic_and_CRT/CRT/run21_cosmic_all_CRT_and_CRP/write_timestemp.C
@@ -0,0 +1,19 @@
+#define memorydepth 2500
+#include "/home/henrique/Dropbox/APC_Paris/Root/cold_box_analysis/class/MYCODES.h"
+
+
+void write_timestemp(){
+  ANALYZER z("z");
+  z.setAnalyzer();
+
+  ofstream fout;
+  fout.open("PDS_TriggerTimes.txt",ios::out);
+
+  fout << "# event,\ttime (s)\n";
+  // for(Int_t i = 0; i < 265; i++){
+  for(Int_t i = 0; i < z.nentries; i++){
+    z.getWaveform(i,0);
+    fout << (int)z.ch[0].event << ",\t" << z.ch[0].time << "\n";
+  }
+  fout.close();
+}
