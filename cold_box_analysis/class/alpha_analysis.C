29e1758a6304e6b4bdd19112fa61ebc3202836da
Henrique Souza PC casa <henriquevieira.souza@gmail.com>
2023-03-10 02:05:56 +0100

    implementation of the new class

--- cold_box_analysis/class/alpha_analysis.C
+++ cold_box_analysis/class/alpha_analysis.C
@@ -188,7 +188,7 @@ void takeCharge(string filename){
   
   TFile *f1 = new TFile(rootfile.c_str(),"READ");
   TTree *t1 = (TTree*)f1->Get("t1");
-  vector<ADC_DATA<memorydepth>> ch(channels.size());
+  vector<ADC_DATA> ch(channels.size());
   vector<TBranch*> bch(channels.size());
   for(Int_t k = 0; k<(int)channels.size();k++){
     bch[k] = t1->GetBranch(Form("Ch%i",channels[k]));
