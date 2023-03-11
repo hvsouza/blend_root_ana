965b3a0dff288267dcbeb78a1fc821ae839f8f34
Henrique Souza <henriquevieira.souza@gmail.com>
2023-03-11 18:43:55 +0100

    Quick update

--- i_have_a_dream/example_tree_read.cxx
+++ i_have_a_dream/example_tree_read.cxx
@@ -1,6 +1,6 @@
 //
 // Usage: root -b -q example_tree_read.cxx
-//        root -b -q -e '.L MY_DATA.hxx' example_tree.root -e 't1->Print();'
+//        root -b -q -e '.L ADC_DATA.hxx' example_tree.root -e 't1->Print();'
 //
 
 #include "../cold_box_analysis/class/MYCODES.h"
@@ -11,7 +11,7 @@
 #include <iostream>
 
 void example_tree_read() {
-  MY_DATA *ch0 = 0, *ch1 = 0; // note: they must be initialized
+  ADC_DATA *ch0 = 0, *ch1 = 0; // note: they must be initialized
   TFile *f1 = TFile::Open("example_tree.root", "READ");
   if ((!f1) || f1->IsZombie()) { delete f1; return; } // just a precaution
   TTree *t1; f1->GetObject("t1", t1);
@@ -24,9 +24,8 @@ void example_tree_read() {
   b1->SetAddress(&ch0);
   b2->SetAddress(&ch1);
   for(Long64_t i = 0; i < t1->GetEntries(); i++) {
-    b1->GetEntry(i);
-    b2->GetEntry(i);
-    if (!(ch0 && ch1)) continue; // just a precaution
+    b1->GetEvent(i);
+    b2->GetEvent(i);
     std::cout << "\nEntry : " << i << "\n";
     // ch0
     std::cout << "ch0 : " << ch0->peak << " " << ch0->selection << " " << ch0->npts << " :";
@@ -40,6 +39,4 @@ void example_tree_read() {
   std::cout << "\n";
   // cleanup
   delete f1; // automatically deletes the "t1", too
-  delete ch0;
-  delete ch1;
 }
